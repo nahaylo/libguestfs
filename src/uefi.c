f9240be59c65e7ed79a18b184cedf34b5878ecfd
Richard W.M. Jones <rjones@redhat.com>
2015-04-30 17:20:26 -0400

    RHEL 7: v2v: efi: Remove references to Fedora / kraxel's OVMF packages.

--- src/uefi.c
+++ src/uefi.c
@@ -30,11 +30,6 @@
 /* See src/appliance.c:guestfs_int_get_uefi. */
 struct uefi_firmware
 guestfs_int_ovmf_i386_firmware[] = {
-  /* kraxel's old repository, these will be removed by end of 2016. */
-  { "/usr/share/edk2.git/ovmf-ia32/OVMF_CODE-pure-efi.fd",
-    NULL,
-    "/usr/share/edk2.git/ovmf-ia32/OVMF_VARS-pure-efi.fd" },
-
   { NULL }
 };
 
@@ -48,15 +43,6 @@ guestfs_int_ovmf_x86_64_firmware[] = {
     NULL,
     "/usr/share/edk2/ovmf/OVMF_VARS.fd" },
 
-  /* kraxel's old repository, these will be removed by end of 2016. */
-  { "/usr/share/edk2.git/ovmf-x64/OVMF_CODE-pure-efi.fd",
-    NULL,
-    "/usr/share/edk2.git/ovmf-x64/OVMF_VARS-pure-efi.fd" },
-
-  { "/usr/share/qemu/ovmf-x86_64-code.bin",
-    NULL,
-    "/usr/share/qemu/ovmf-x86_64-vars.bin" },
-
   { NULL }
 };
 
@@ -69,11 +55,6 @@ guestfs_int_aavmf_firmware[] = {
   { "/usr/share/edk2/aarch64/QEMU_EFI-pflash.raw",
     NULL,
     "/usr/share/edk2/aarch64/vars-template-pflash.raw" },
-
-  /* kraxel's old repository, these will be removed by end of 2016. */
-  { "/usr/share/edk2.git/aarch64/QEMU_EFI-pflash.raw",
-    NULL,
-    "/usr/share/edk2.git/aarch64/vars-template-pflash.raw" },
 
   { NULL }
 };
